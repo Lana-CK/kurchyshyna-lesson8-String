2807f36261f286b1f83052cee0d17b75ea25a43c
Sviatlana Kurchyshyna <lana2034@yandex.ru>
2021-09-28 00:19:35 +0300

    add Task 7 (formatted)

--- lesson8/src/com/belhard/strings/Task7.java
+++ lesson8/src/com/belhard/strings/Task7.java
@@ -6,23 +6,15 @@ public class Task7 {
 
 	public static void main(String[] args) {
 		String str7 = "And sorry I could not travel both. And be one traveler, long I stood";
-		   StringBuilder str1 = new StringBuilder(str7);
-		   StringBuilder str2 = new StringBuilder();
-	        for (int i = 0; i < str1.length(); i++) {
-	            str2.append(str1.charAt(i));
-	            if (str1.charAt(i) == 'a') str2.append("b");    // charAt(int index) 
-	            if (str1.charAt(i) == 'A') str2.append("B");
-	        }
-		System.out.println("String where bB goes after aA: \n"+ str2.toString());
-	
-//		String regex = "([Aa+])";
-//		String replacement = "Bb";
-//		str7 = str7.replaceAll(regex, replacement);
-//		return str7;
-//		
-	        }
+		StringBuilder str1 = new StringBuilder(str7);
+		StringBuilder str2 = new StringBuilder();
+		for (int i = 0; i < str1.length(); i++) {
+			str2.append(str1.charAt(i));
+			if (str1.charAt(i) == 'a')
+				str2.append("b"); // charAt(int index)
+			if (str1.charAt(i) == 'A')
+				str2.append("B");
+		}
+		System.out.println("String where bB goes after aA: \n" + str2.toString());
 	}
-
-		
-	        
-	    
+}
